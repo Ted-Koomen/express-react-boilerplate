8a15798eab4ca5bf0215d45ace5e91d8f6cfc973
Ted Koomen <koomen.ted@gmail.com>
2021-01-22 20:24:20 -0500

    pseudocode for s3

--- server/index.js
+++ server/index.js
@@ -1,46 +1,53 @@
 const express = require('express');
 const app = express();
+const AWS = require('aws-sdk')
 const streamingClient = require('./axiosStreamClient');
 const axios = require('axios')
 const httpAdapter = require('axios/lib/adapters/http')
 const fs = require('fs');
 
+const s3 = new AWS.S3({
+    accessKeyId: process.env.S3ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+})
 
-app.get('/', async () => {
+const uploadToS3 = async (fileId) => {
+    const params = {
+        Bucket: process.env.UPLOAD_BUCKET,
+        Key: file.id,
+        Body: fs.readFileSync(pathToFile)
+    }
+
+    const data = await s3.upload(params)
+    return data.location;
+}
+
+app.get('/api/art/:query', async (req, res) => {
     try {
-        const output = fs.createWriteStream('image.jpg')
-
-        const res = await axios.get('https://images.metmuseum.org/CRDImages/ep/original/DP346474.jpg',
-            { responseType: 'stream', adapter: httpAdapter }
-        );
-
-        const stream = res.data;
-        stream.on('data', chunk => {
-            output.write(new Buffer.from(chunk))
+        // make call to external API
+        const response = await axios.get('/api/art/download/:download_url')
+        const location = await axios.post('/api/upload', {
+            params: {
+                fileId: response
+            }
         })
-
-        stream.on('end', () => {
-            output.end()
-        })
-
+        
+        res.send(location)
     } catch (e) {
-        console.log(e)
+        throw new Error(e)
     }
 })
 
-app.listen(5000, () => {
-    console.log("Listening on 5000")
-});
-
-
-/*
-app.get('/api/art/:query', async (req, res) => {
-    //make api call to external api
-    const response = await axios.get('/api/art/download/:download_url')
-    res.send(response)
+// private route so randos cant upload
+app.post('/api/upload/:fileId', async (req, res) => {
+    try {
+        const response = await uploadToS3(req.params.fileId)
+        return response
+    } catch (e) {
+        throw new Error(e)
+    }
 })
 
-// for future make this a private route so bad actors can't blow up your shit easily
 app.get('/api/art/download/:download_url', async (req, res) => {
     try {
         const output = fs.createWriteStream('image.jpg')
@@ -63,4 +70,7 @@ app.get('/api/art/download/:download_url', async (req, res) => {
     }
 
 })
-*/
+
+app.listen(5000, () => {
+    console.log("Listening on 5000")
+});
