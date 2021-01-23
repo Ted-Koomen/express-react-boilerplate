ee42766169b8da0441177e91f350eb0e000ff5cc
Ted Koomen <koomen.ted@gmail.com>
2021-01-22 19:20:13 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const httpAdapter = require('axios/lib/adapters/http')
+const fs = require('fs');
+
+
+app.get('/', async (req, res) => {
+    try {
+        const output = fs.createWriteStream('image.jpg')
+
+        axios.get(
+            'https://images.metmuseum.org/CRDImages/ep/original/DP346474.jpg',
+            {
+                responseType: 'stream',
+                adapter: httpAdapter
+            }
+        )
+            .then(res => {
+                const stream = res.data;
+                stream.on('data', chunk => {
+                    console.log('writing')
+                    output.write(new Buffer.from(chunk))
+                })
+
+                stream.on('end', () => {
+                    console.log("done")
+                    output.end()
+                })
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+})
 
 app.listen(5000, () => {
     console.log("Listening on 5000")
