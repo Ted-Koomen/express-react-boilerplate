3be499512dbe73068864b4aa024029dc6e6394b4
Ted Koomen <koomen.ted@gmail.com>
2021-01-22 20:01:14 -0500

    typo

--- server/axiosStreamClient.js
+++ server/axiosStreamClient.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const httpAdapter = requir('axios/lib/adapters/http');
+const httpAdapter = require('axios/lib/adapters/http');
 
 export default axios.create({
     responseType: 'stream',
